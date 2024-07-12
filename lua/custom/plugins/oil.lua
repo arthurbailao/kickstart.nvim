5f2c84eea5e120e59928b8874e1437c16148fda3
Arthur Bailão <arthurbailao@gmail.com>
2024-07-12 16:43:38 -0300

    improving oil config

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,26 +1,32 @@
 return {
   'stevearc/oil.nvim',
-  opts = {
-    view_options = {
-      -- Show files and directories that start with "."
-      show_hidden = true,
-      -- This function defines what is considered a "hidden" file
-      is_hidden_file = function(name, _)
-        return vim.startswith(name, '.')
-      end,
-      -- This function defines what will never be shown, even when `show_hidden` is set
-      is_always_hidden = function(name, _)
-        return name == '.git' and vim.fn.isdirectory(name)
-      end,
-    },
-  },
-
-  init = function()
-    vim.keymap.set('n', '<leader>nn', function()
-      require('oil').open(nil)
-    end, { desc = 'Oil Open' })
-  end,
-
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+  config = function()
+    require('oil').setup {
+      columns = { 'icon' },
+      keymaps = {
+        ['<C-h>'] = false,
+        ['<C-l>'] = false,
+        ['<C-r>'] = 'actions.refresh',
+      },
+
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, _)
+          return vim.startswith(name, '.')
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, _)
+          return name == '.git' and vim.fn.isdirectory(name)
+        end,
+      },
+    }
+
+    vim.keymap.set('n', '<leader>nn', '<CMD>Oil<CR>', { desc = 'Oil Open' })
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Oil Open' })
+  end,
 }
