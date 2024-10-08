9aa977bade2069238bda9ab4f44355576e48c944
Arthur Bailão <arthurbailao@gmail.com>
2024-10-07 22:10:44 -0300

    replace markdown plugin

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,5 +1,10 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  opts = {},
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  'OXY2DEV/markview.nvim',
+  lazy = false, -- Recommended
+  -- ft = "markdown" -- If you decide to lazy-load anyway
+
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
 }
