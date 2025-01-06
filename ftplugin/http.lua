f8f6c6de1ca8729a8184a02067391b77ec2a79c2
Arthur Bailão <arthurbailao@gmail.com>
2025-01-05 21:39:32 -0300

    kulala keymaps

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -1,3 +1,17 @@
 vim.api.nvim_set_keymap('n', '<C-p>', ":lua require('kulala').jump_prev()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-n>', ":lua require('kulala').jump_next()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-y>', ":lua require('kulala').run()<CR>", { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(
+  0,
+  'n',
+  '<leader>co',
+  "<cmd>lua require('kulala').copy()<cr>",
+  { noremap = true, silent = true, desc = 'Copy the current request as a curl command' }
+)
+vim.api.nvim_buf_set_keymap(
+  0,
+  'n',
+  '<leader>ci',
+  "<cmd>lua require('kulala').from_curl()<cr>",
+  { noremap = true, silent = true, desc = 'Paste curl from clipboard as http request' }
+)
