93940e7617febb30680bd1ef901211dbf3575fa7
Arthur Bailão <arthurbailao@gmail.com>
2025-07-30 15:42:26 -0300

    markview completion

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -2,9 +2,9 @@ return {
   'OXY2DEV/markview.nvim',
   lazy = false, -- Recommended
   -- ft = "markdown" -- If you decide to lazy-load anyway
-
+  priority = 49,
+  -- For blink.cmp's completion
   dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-web-devicons',
+    'saghen/blink.cmp',
   },
 }
